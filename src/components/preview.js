ee3c086d6fcad46f51034141be27c3c2fce10aae
mijime <07cc0110+gthb@gmail.com>
2017-03-31 03:30:28 +0900

    :wrench: Use remark-emoji and rehype-prism

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,6 +1,9 @@
 import React from "react";
-import marked from "marked";
-import prism from "prismjs";
+import remark from "remark";
+import emoji4remark from "remark-emoji";
+import remark2rehype from "remark-rehype";
+import rehype2react from "rehype-react";
+import prism4rehype from "../utils/rehype-prism";
 import "prismjs/components/prism-java";
 import "prismjs/components/prism-json";
 import "prismjs/components/prism-go";
@@ -9,35 +12,20 @@ import "prismjs/components/prism-yaml";
 import "prismjs/components/prism-sql";
 import "prismjs/themes/prism.css";
 
-class CodeRenderer extends marked.Renderer {
-    code(literal, language) {
-        const grammer = prism.languages[language];
-
-        if (grammer) {
-            const className = `language-${language}`;
-
-            return `<pre className=${className}><code className=${className}>${
-                prism.highlight(
-                  literal,
-                  grammer,
-                  language
-                )
-            }</code></pre>`;
-        }
-
-        return super.code(literal, language);
-    }
-}
-
-const renderer = new CodeRenderer();
+const processor = remark()
+    .use(emoji4remark)
+    .use(remark2rehype)
+    .use(prism4rehype)
+    .use(rehype2react, {
+        createElement: React.createElement
+    });
 
 /**
  * @param {string} source is markdown raw text
  * @returns {ReactMarkdown} react markdown component
  **/
 export default function render({ className, source }) {
-    return <div className={className}
-        dangerouslySetInnerHTML={{
-            __html: marked(source, { renderer })
-        }} />;
+    const { contents } = processor.processSync(source);
+
+    return <div className={className}>{contents}</div>;
 }
