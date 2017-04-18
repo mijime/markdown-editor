660b3fcefb3d7fb37c8d45eb9cb56e5467d8b936
mijime <07cc0110+gthb@gmail.com>
2017-04-19 08:41:05 +0900

    :bug: Apply `eslint --fix`

--- src/libs/rehype-emojione.js
+++ src/libs/rehype-emojione.js
@@ -2,74 +2,76 @@
 import emojione from "emojione";
 
 const NodeTypes = {
-  TEXT: "text",
-  ELEMENT: "element"
-}
+    TEXT: "text",
+    ELEMENT: "element"
+};
 
 const NodeTagNames = {
-  PARAGRAPH: "p",
-  SPAN: "span",
-  IMAGE: "img"
-}
+    PARAGRAPH: "p",
+    SPAN: "span",
+    IMAGE: "img"
+};
 
 function sliceToEmoji(nodes, text) {
-  const sliced = emojione.regShortNames.exec(text);
-  console.log(text, sliced);
+    const sliced = emojione.regShortNames.exec(text);
 
-  if (!sliced) {
-    nodes.push({
-      type: NodeTypes.TEXT,
-      value: text
-    });
-    return nodes;
-  }
+    console.log(text, sliced);
 
-  const shortname = sliced[0];
-  const unicode = emojione.emojioneList[shortname]
+    if (!sliced) {
+        nodes.push({
+            type: NodeTypes.TEXT,
+            value: text
+        });
+        return nodes;
+    }
+
+    const shortname = sliced[0];
+    const unicode = emojione.emojioneList[shortname]
     .unicode[emojione.emojioneList[shortname]
       .unicode.length - 1];
 
-  if (sliced.index > 0) {
-    nodes.push({
-      type: NodeTypes.TEXT,
-      value: text.substr(0, sliced.index)
-    });
-  }
-
-  nodes.push({
-    tagName: NodeTagNames.IMAGE,
-    type: NodeTypes.ELEMENT,
-    properties: {
-      class: "emojione",
-      alt: emojione.unicodeAlt ? emojione.convert(unicode.toUpperCase()) : shortname,
-      title: shortname,
-      src: `${emojione.imagePathPNG}${unicode}.png${emojione.cacheBustParam}`
+    if (sliced.index > 0) {
+        nodes.push({
+            type: NodeTypes.TEXT,
+            value: text.substr(0, sliced.index)
+        });
     }
-  });
 
-  const nextText = text.substr(
+    nodes.push({
+        tagName: NodeTagNames.IMAGE,
+        type: NodeTypes.ELEMENT,
+        properties: {
+            class: "emojione",
+            alt: emojione.unicodeAlt ? emojione.convert(unicode.toUpperCase()) : shortname,
+            title: shortname,
+            src: `${emojione.imagePathPNG}${unicode}.png${emojione.cacheBustParam}`
+        }
+    });
+
+    const nextText = text.substr(
     sliced.index + shortname.length,
     text.length);
-  return sliceToEmoji(nodes, nextText);
+
+    return sliceToEmoji(nodes, nextText);
 }
 
 export default function plugin(settings) {
-  function transformer(node) {
-    if (node.type !== NodeTypes.TEXT) {
-      if (!node.children) {
-        return;
-      }
+    function transformer(node) {
+        if (node.type !== NodeTypes.TEXT) {
+            if (!node.children) {
+                return;
+            }
 
-      node.children.map(transformer);
-      return;
+            node.children.map(transformer);
+            return;
+        }
+
+        node.children = sliceToEmoji([], node.value);
+        node.tagName = NodeTagNames.SPAN;
+        node.type = NodeTypes.ELEMENT;
+        delete node.value;
+        console.log(node);
     }
 
-    node.children = sliceToEmoji([], node.value);
-    node.tagName = NodeTagNames.SPAN;
-    node.type = NodeTypes.ELEMENT;
-    delete node.value;
-    console.log(node);
-  }
-
-  return transformer;
+    return transformer;
 }
