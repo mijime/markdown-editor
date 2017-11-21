0e7df903493e2147b5f990a701ba7430166a79e9
mijime <07cc0110+gthb@gmail.com>
2017-11-22 08:32:05 +0900

    Fix selection element

--- src/components/preview.js
+++ src/components/preview.js
@@ -15,39 +15,95 @@ import styles from "../styles/main.sass";
 
 let currentSelection = null;
 
-/** *
- * @param {Node} node is node
- * @returns {void}
- **/
-function walk(node) {
-    if (!node || !node.position) {
-        return;
+const customProcessor = PreviewProcessor.use(() => {
+
+    /** *
+     * @param {Node} node is node
+     * @returns {void}
+     **/
+    function transfer(node) {
+        if (!currentSelection) {
+            return node;
+        }
+
+        if (!node || !node.position) {
+            return node;
+        }
+
+        const { start, end } = node.position;
+
+        if (
+            node.type === "element" &&
+            start.offset < currentSelection.selectionStart &&
+            end.offset > currentSelection.selectionEnd &&
+            node.children.every(child => child.type !== "element")
+        ) {
+            if (node.properties) {
+                node.properties.className = styles.cursor;
+            } else {
+                node.properties = { className: styles.cursor };
+            }
+            return node;
+        }
+
+        if (!node.children) {
+            return node;
+        }
+
+        node.children.map(transfer);
+        return node;
+    }
+    return transfer;
+}).use(() => {
+
+    /**
+     * @param {Node} node is node
+     * @returns {boolean} is empty
+     **/
+    function isList(node) {
+        return node.type === "element" && node.tagName === "li";
     }
 
-    const { start, end } = node.position;
+    /**
+     * @param {Node} node is node
+     * @returns {boolean} is empty
+     **/
+    function hasAnyChildren(node) {
+        if (!node.children || node.children.length === 0) {
+            return true;
+        }
 
-    if (
-        node.type === "element" &&
-        start.offset < currentSelection.selectionStart &&
-        end.offset > currentSelection.selectionEnd
-    ) {
-        node.properties.className = styles.cursor;
-        return;
+        if (node.tagName !== "span") {
+            return true;
+        }
+
+        return !node.children.every(
+            child => child.type === "text" && child.value === "\n"
+        );
     }
 
-    if (!node.children) {
-        return;
+    /**
+     * @param {Node} node is node
+     * @returns {Node} is empty
+     **/
+    function transfer(node) {
+        if (!node.children || node.children.length === 0) {
+            return node;
+        }
+
+        if (node.tagName === "ul") {
+            node.children = node.children
+                .filter(hasAnyChildren)
+                .filter(isList)
+                .map(transfer);
+            return node;
+        }
+
+        node.children = node.children.filter(hasAnyChildren).map(transfer);
+        return node;
     }
 
-    node.children.map(walk);
-}
-
-const customProcessor = PreviewProcessor.use(() => node => {
-    if (!currentSelection) {
-        return;
-    }
-
-    walk(node);
+    return transfer;
 });
 
 /**
